d15e5eb418987d413e1af68c608432c5edb051f0
Matheus Martins <xmatheusclm@gmail.com>
2024-11-11 21:26:08 -0300

    erros resolvido indo para Admin!

--- galeria/views.py
+++ galeria/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from galeria.models import Fotografia
+from django.shortcuts import get_object_or_404
 
 def index(request):
     fotografias = Fotografia.objects.all()
     return render (request, 'galeria/index.html', {'cards': fotografias}  )
 
-def imagem(request):
-    return render (request, 'galeria/imagem.html')
+def imagem(request, foto_id):
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)
+    return render (request, 'galeria/imagem.html', {"fotografia": fotografia})
