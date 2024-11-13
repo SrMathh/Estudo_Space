9155f08d40734dfa811ddf9097e44f4a781d0608
Matheus Martins <xmatheusclm@gmail.com>
2024-11-13 16:30:32 -0300

    03 avnaçando no admin

--- galeria/models.py
+++ galeria/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
 class Fotografia(models.Model):
+
+    OPCOES_CATEGORIA = [
+        ('NEBULOSA','Nebulosa'),
+        ('ESTRELA','Estrela'),
+        ('GALÁXIA','Galaxia'),
+        ('PLANETA','Planeta'),
+    ]
+
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.TextField(max_length=100, null=False, blank=False)
+    categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORIA, default='')
     descricao = models.TextField(null=False, blank=False)
     foto = models.ImageField(max_length=100, null=False, blank=False)
     def __str__(self):
