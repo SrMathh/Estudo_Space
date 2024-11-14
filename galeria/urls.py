937b4594df3cf23e9b97584e9e1033fb2b74dd36
Matheus Martins <xmatheusclm@gmail.com>
2024-11-14 15:22:08 -0300

    final do curso dados e admin

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,7 +1,8 @@
 from django.urls import include, path
-from galeria.views import index, imagem
+from galeria.views import index, imagem, buscar
 
 urlpatterns = [
     path('', index, name = 'index'),
-    path('imagem/<int:foto_id>', imagem, name = 'imagem')
+    path('imagem/<int:foto_id>', imagem, name = 'imagem'),
+    path('buscar', buscar, name='buscar'),
 ]
